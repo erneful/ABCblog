b6c4bcf7d3c0fd392c6b3796bb48e655b7e7c440
ernefull <ernefull6@gmail.com>
2023-10-30 04:42:30 -0400

    ABCblog/Setting.py

--- ABCblog/settings.py
+++ ABCblog/settings.py
@@ -181,11 +181,11 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # configurar email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bandapiace@gmail.com'
+EMAIL_HOST_PASSWORD = 'iyknpjooakusixbp'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
