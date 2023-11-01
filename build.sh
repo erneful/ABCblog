12be9977e7857e7ce301c27cd547c98763c9178c
ernefull <ernefull6@gmail.com>
2023-11-01 02:23:33 -0400

    first commit

--- build.sh
+++ build.sh
@@ -7,4 +7,15 @@ pip install -r requirements.txt
 
 python app/manage.py check --deploy --fail-level ERROR
 python app/manage.py collectstatic --no-input
-python app/manage.py migrate
+p#!/usr/local/bin/python3.9.5
+
+#-*- coding: utf-8 -*-
+import re
+import sys
+
+from gunicorn.app.wsgiapp import run
+
+
+if __name__ == '__main__':
+        sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$','',sys.argv[0])
+        sys.exit(run())ython app/manage.py migrate
